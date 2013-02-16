36c469f9d2b2d3dd06b14b3e744b94b482deb704
Tim Craft <mail@timcraft.com>
2013-02-16 15:19:36 +0000

    Fix entity spec descriptions for consistency (less shoulding)

--- spec/entity_spec.rb
+++ spec/entity_spec.rb
@@ -1,73 +1,73 @@
 require 'minitest/autorun'
 require 'hom'
 
-describe 'Named entity' do
+describe 'HOM::Entity initialized with a symbol' do
   before do
     @entity = HOM::Entity.new(:nbsp)
   end
 
   describe 'value method' do
-    it 'should return the symbol value' do
+    it 'returns the symbol value' do
       @entity.value.must_equal(:nbsp)
     end
   end
 
   describe 'numeric query method' do
-    it 'should return false' do
+    it 'returns false' do
       @entity.numeric?.must_equal(false)
     end
   end
 
   describe 'named query method' do
-    it 'should return true' do
+    it 'returns true' do
       @entity.named?.must_equal(true)
     end
   end
 
   describe 'to_s method' do
-    it 'should return the encoded html entity' do
+    it 'returns the encoded html entity' do
       @entity.to_s.must_equal('&nbsp;')
     end
   end
 
   describe 'html_safe query method' do
-    it 'should return true' do
+    it 'returns true' do
       @entity.html_safe?.must_equal(true)
     end
   end
 end
 
-describe 'Numeric entity' do
+describe 'HOM::Entity initialized with an integer' do
   before do
     @entity = HOM::Entity.new(160)
   end
 
   describe 'value method' do
-    it 'should return the integer value' do
+    it 'returns the integer value' do
       @entity.value.must_equal(160)
     end
   end
 
   describe 'numeric query method' do
-    it 'should return true' do
+    it 'returns true' do
       @entity.numeric?.must_equal(true)
     end
   end
 
   describe 'named query method' do
-    it 'should return false' do
+    it 'returns false' do
       @entity.named?.must_equal(false)
     end
   end
 
   describe 'to_s method' do
-    it 'should return the encoded html entity' do
+    it 'returns the encoded html entity' do
       @entity.to_s.must_equal('&#160;')
     end
   end
 
   describe 'html_safe query method' do
-    it 'should return true' do
+    it 'returns true' do
       @entity.html_safe?.must_equal(true)
     end
   end
